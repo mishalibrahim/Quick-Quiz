b7e17d52bab734190c520dc2640812984e882101
Mishal Ibrahim <mishalrehan123@gmail.com>
2024-11-02 19:52:25 +0530

    loading page ui enhancement
    added color and updated fontweight

--- app/result/loading.js
+++ app/result/loading.js
@@ -1,7 +1,7 @@
 export default function Loading(){
   return (
-    <div className='flex justify-center items-center w-full h-full'>
-      <p className="text-[20px]">
+    <div className='flex justify-center items-center h-screen bg-white'>
+      <p className="text-[16px] sm:text-2xl text-text_primary font-bold">
         Calculating Your Results Please wait
       </p>
     </div>
